6468031332516a97ff2161e7e939a73921ac241c
NurikN1 <baykenov2000icloud.com>
2022-06-07 23:01:26 +0600

    feature: add products

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,25 +1,50 @@
 import productImage from '../assets/images/product-card.png';
 import { ReactComponent as FavouriteIcon } from '../assets/icons/favourite.svg';
 
+interface IProduct {
+  id: string;
+  name: string;
+  places: string;
+  price: string;
+  address: string;
+  description: string;
+  imgUrl: string;
+}
+
 type Props = {
   showFavourite?: boolean;
+  product?: IProduct;
 };
 
-const ProductCard = ({ showFavourite = false }: Props) => {
+const ProductCard = ({
+  showFavourite = false,
+  product = {
+    id: 'rock',
+    name: 'Rock Crystal',
+    places: '120',
+    price: '9000–12000 тенге на человека',
+    address: 'пр. Гагарина, 2, уг. ул. Толе би',
+    description:
+      'Сцена, танцпол, зона для курящих, гримёрная, звуковая аппаратура, световая аппаратура, музыкальное сопровождение, ведущий (тамада), фотосъёмка, видеосъёмка, оформление зала',
+    imgUrl: '/images/product-card.png',
+  },
+}: Props) => {
   return (
     <div className="relative inline-flex h-[360px]">
       <div className="w-[300px] bg-background rounded-tl-2xl rounded-bl-2xl">
-        <img src={productImage} alt="Product Image" className="w-full h-full rounded-2xl" />
+        <img
+          src={process.env.PUBLIC_URL + product.imgUrl}
+          alt="Product Image"
+          className="w-full h-full rounded-2xl"
+        />
       </div>
       <div className="w-[320px] pt-6 pr-6 pl-7 bg-background rounded-tr-2xl rounded-br-2xl">
         <h4 className="mb-4 text-2xl font-semibold text-black-text leading-tight truncate">
-          Rock Crystal
+          {product.name}
         </h4>
-        <p className="mb-4 text-base leading-tight text-black-text">120 мест</p>
-        <p className="mb-4 text-base leading-tight text-black-text">9000–12000 тенге на человека</p>
-        <p className="mb-4 text-base leading-tight text-grey-text truncate">
-          пр. Гагарина, 2, уг. ул. Толе би
-        </p>
+        <p className="mb-4 text-base leading-tight text-black-text">{product.places}</p>
+        <p className="mb-4 text-base leading-tight text-black-text">{product.price}</p>
+        <p className="mb-4 text-base leading-tight text-grey-text truncate">{product.address}</p>
         <p
           className="mb-4 text-base leading-normal text-black-text overflow-hidden text-ellipsis"
           style={{
@@ -27,8 +52,7 @@ const ProductCard = ({ showFavourite = false }: Props) => {
             WebkitLineClamp: 7,
             WebkitBoxOrient: 'vertical',
           }}>
-          Сцена, танцпол, зона для курящих, гримёрная, звуковая аппаратура, световая аппаратура,
-          музыкальное сопровождение, ведущий (тамада), фотосъёмка, видеосъёмка, оформление зала
+          {product.description}
         </p>
       </div>
       {showFavourite && (
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
 import { ReactComponent as SearchIcon } from '../assets/icons/search.svg';
 import ProductCard from '../components/ProductCard';
+import { NEW_ARTISTS } from '../data/artists';
+import { NEW_RESTAURANTS } from '../data/products';
 
 const Home = () => {
+  const newRestaurants = NEW_RESTAURANTS;
+
   return (
     <div className="pt-6 pb-24">
       <div className="relative container mx-auto mb-16 flex h-[600px]">
@@ -9,9 +14,11 @@ const Home = () => {
           <h2 className="mb-6 text-5xl font-bold text-white">
             Создай свой собственный праздник вместе с нами!
           </h2>
-          <button className="flex items-center justify-center px-4 h-[46px] text-lg font-medium text-white border rounded-lg">
+          <Link
+            to="/catalog/restaurants"
+            className="flex items-center justify-center px-4 w-[96px] h-[46px] text-lg font-medium text-white border rounded-lg">
             Начать
-          </button>
+          </Link>
         </div>
         <div className="hero basis-2/4 h-full bg-cover bg-center rounded-tr-[40px]"></div>
         <div
@@ -33,15 +40,17 @@ const Home = () => {
       <div className="container mx-auto mb-14">
         <h3 className="mb-6 text-headline3 font-bold text-black-text">Новые рестораны</h3>
         <div className="flex justify-between">
-          <ProductCard />
-          <ProductCard />
+          {newRestaurants.map((restaurant) => (
+            <ProductCard key={restaurant.id} product={restaurant} />
+          ))}
         </div>
       </div>
       <div className="container mx-auto">
         <h3 className="mb-6 text-headline3 font-bold text-black-text">Новые исполнители</h3>
         <div className="flex justify-between">
-          <ProductCard />
-          <ProductCard />
+          {NEW_ARTISTS.map((artist) => (
+            <ProductCard key={artist.id} product={artist} />
+          ))}
         </div>
       </div>
     </div>
